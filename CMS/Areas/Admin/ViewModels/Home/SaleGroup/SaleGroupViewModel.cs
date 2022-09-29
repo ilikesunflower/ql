fe9493315999debcda894eb6b6bc1c149bdf3c7e
cesc bui <buiduc184@gmail.com>
2022-09-29 17:48:39 +0700

    cap nhat

--- CMS/Areas/Admin/ViewModels/Home/SaleGroup/SaleGroupViewModel.cs
+++ CMS/Areas/Admin/ViewModels/Home/SaleGroup/SaleGroupViewModel.cs
@@ -1,7 +1,11 @@
+using CMS.Extensions.Validate;
+
 namespace CMS.Areas.Admin.ViewModels.Home.SaleGroup;
 
 public class SaleGroupViewModel
 {
+    [ValidXss]
     public string DateStart { get; set; }
+    [ValidXss]
     public string DateEnd { get; set; }
 }
--- CMS/Areas/Admin/ViewModels/Home/ToProduct/ToProductViewModel.cs
+++ CMS/Areas/Admin/ViewModels/Home/ToProduct/ToProductViewModel.cs
@@ -1,8 +1,12 @@
+using CMS.Extensions.Validate;
+
 namespace CMS.Areas.Admin.ViewModels.Home.ToProduct;
 
 public class ToProductViewModel
 {
     public int FilterStatus { get; set; }
+    [ValidXss]
     public string DateStart { get; set; }
+    [ValidXss]
     public string DateEnd { get; set; }
 }
