3e9354d450998e9e8d0fc91f963316a3299ae263
cesc bui <buiduc184@gmail.com>
2022-10-06 11:13:25 +0700

    fix lôi xss

--- CMS/Areas/Admin/ViewModels/Home/SaleGroup/SaleGroupViewModel.cs
+++ CMS/Areas/Admin/ViewModels/Home/SaleGroup/SaleGroupViewModel.cs
@@ -1,15 +1,9 @@
-using System.ComponentModel.DataAnnotations;
 using CMS.Extensions.Validate;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace CMS.Areas.Admin.ViewModels.Home.SaleGroup;
 
 public class SaleGroupViewModel
 {
-    [ValidXss]
-    public string DateStart { get; set; }
-    [ValidXss]
-    public string DateEnd { get; set; }
-    
+    [ValidXss] public string DateStart { get; set; }
+    [ValidXss] public string DateEnd { get; set; }
 }
