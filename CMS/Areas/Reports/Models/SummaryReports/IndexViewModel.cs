5a93c3c7cb0f6baefdf7200b7b1e99f398674b66
cesc bui <buiduc184@gmail.com>
2022-10-13 14:15:48 +0700

    fix lỗi ng dung

--- CMS/Areas/Reports/Models/SummaryReports/IndexViewModel.cs
+++ CMS/Areas/Reports/Models/SummaryReports/IndexViewModel.cs
@@ -8,4 +8,6 @@ public class IndexViewModel
     public PagingList<CMS_EF.Models.Orders.Orders> ListData { get; set; }
     public Dictionary<int,string> OrderStatusPayments { get; set; }
     public Dictionary<int,string> ListStatus { get; set; }
+    
+    public bool IsDetailOrder { get; set; }
 }
